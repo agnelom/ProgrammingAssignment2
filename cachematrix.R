c9126bd46375b7b1a21fa99624c690c3f9766e7d
agnelom <agnelom@hotmail.com>
2015-10-25 13:29:12 +0530

    Final version with well formated code '

--- cachematrix.R
+++ cachematrix.R
@@ -10,17 +10,16 @@
 ## to cache the inverse of the matrix and provide related methods to set and get data
 
 makeCacheMatrix <- function(x = matrix()) {
-  mInv <- NULL
-  set <- function(y) {
-      x <<- y
-      mInv <<- NULL
-  }
-  get <- function() x
-  setInv <- function(inv) mInv <<- inv
-  getInv <- function() mInv
-  list(set = set, get = get, setInv = setInv, getInv = getInv)
+    mInv <- NULL
+    set <- function(y) {
+        x <<- y
+        mInv <<- NULL
+    }
+    get <- function() x
+    setInv <- function(inv) mInv <<- inv
+    getInv <- function() mInv
+    list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
-
 
 ## The cacheSolve function takes as input what is output by makeCacheMatrix
 ## this function is responsible for calculating the inverse of the special matrix create by makeCacheMatrix
@@ -28,19 +27,19 @@ makeCacheMatrix <- function(x = matrix()) {
 ## in also puts the value into the cache using the setInv function in makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  nInv <- x$getInv()
-  if(!is.null(nInv)) {
-    print("Getting inverse from Cache")
-    return(nInv)
-  }
-  data <- x$get()
-  if (nrow(data)==ncol(data)){
-    nInv <- solve(data)
-  }
-  else {
-    print("Only Square matrix supported")
-  }
-  x$setInv(nInv)
-  nInv
+    ## Return a matrix that is the inverse of 'x'
+    nInv <- x$getInv()
+    if(!is.null(nInv)) {
+        print("Getting inverse from Cache")
+        return(nInv)
+    }
+    data <- x$get()
+    if (nrow(data)==ncol(data)){
+        nInv <- solve(data)
+    }
+    else {
+        print("Only Square matrix supported")
+    }
+    x$setInv(nInv)
+    nInv
 }
